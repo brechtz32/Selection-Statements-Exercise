3f7903648327b21d0fc25576ff3db3d6e1c575d1
zach brecht <brechtz32@gmail.com>
2024-02-13 21:06:14 -0600

    Selection Statements exercise1

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Number Guessing game
+            var r = new Random();
+            var favNumber = r.Next(1, 1000);
+            
+
+            //user input
+            Console.WriteLine("Pick a number between 1 and 1,000.");
+            var userInput = int.Parse(Console.ReadLine());
+
+
+            //if else statement assesing users input to favNumber
+            if (userInput < favNumber)
+            {
+                Console.WriteLine($"{userInput} is too low.");
+            }
+            else if (userInput > favNumber)
+            {
+                Console.WriteLine($"{userInput} is too high.");
+            }
+            else
+            {
+                Console.WriteLine($"You guessed it! The number was {favNumber}!");
+            }                                                
+
         }
     }
 }
